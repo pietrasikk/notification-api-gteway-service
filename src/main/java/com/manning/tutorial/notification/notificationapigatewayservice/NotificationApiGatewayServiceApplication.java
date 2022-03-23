ca1a33d877a4d6803f1e308bd6e963cccd44279e
Kacper Pietrasik <pietrasikk@gmail.com>
2022-03-23 13:49:39 +0100

    Initial project

---
+++ src/main/java/com/manning/tutorial/notification/notificationapigatewayservice/NotificationApiGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.manning.tutorial.notification.notificationapigatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationApiGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manning/tutorial/notification/notificationapigatewayservice/NotificationApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manning.tutorial.notification.notificationapigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
